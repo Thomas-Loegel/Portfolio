a3611cf6f235b3fe3cc9f7b818c85befee73d3d4
Thomas Loegel <t.loegel@gmail.com>
2020-03-02 17:01:32 +0100

    modal pour le cv

--- js/script.js
+++ js/script.js
@@ -21,6 +21,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+
+
+// Cv Modal
+const modal = document.getElementById('myModal');
+const btn   = document.getElementById('myBtn');
+const span  = document.getElementsByClassName('close')[0];
+
+btn.onclick = function() {
+   modal.style.display = 'block';
+}
+
+span.onclick = function() {
+   modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = 'none';
+   }
+}
+
+
+
+
 // Burger Effect
 // const button = document.querySelector('.burger-button');
 // const onClick = function() {
